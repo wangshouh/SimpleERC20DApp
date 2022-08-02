1394dc87efad7f010b8f3f809bbb66ffee7114cb
Wong Shouhao <wongshouhao@outlook.com>
2022-08-02 22:05:30 +0800

    Add smartcontract vue.

---
+++ src/components/smartcontract.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="card">
+        <div class="content has-text-centered py-4">
+            <p>
+                <button class="button is-info button-width py-2">
+                    <span class="material-icons">
+                        add_circle_outline
+                    </span>
+                    Add new ERC20
+                </button>
+            </p>
+            <p>
+                <button class="button is-info button-width py-2">
+                    <span class="material-icons">
+                        post_add
+                    </span>
+                    Import your ERC20
+                </button>
+            </p>
+        </div>
+
+    </div>
+</template>
+
+<style>
+.button-width {
+    width: 50%;
+}
+</style>
+
+<script setup>
+
+</script>
