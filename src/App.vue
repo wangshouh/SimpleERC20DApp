61771bfba90cb1478f5accbd5f13ad149188ce9e
Wong Shouhao <wongshouhao@outlook.com>
2022-08-02 22:05:51 +0800

    Change columns size.

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import Metamask from './components/metamask.vue';
+import smartcontract from './components/smartcontract.vue';
 </script>
 
 <template>
 <div class="container top-gap">
   <div class="columns">
-    <div class="column is-one-quarter">
+    <div class="column is-3">
       <Metamask />
     </div>
-    <div class="column">Auto</div>
-    <div class="column">Auto</div>
+    <div class="column is-6">
+      <smartcontract />
+    </div>
+    <div class="column"></div>
   </div>
 </div>
 </template>
