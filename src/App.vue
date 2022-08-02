6d2476dda9743497c9b7beee8f3178d5d6fc78e6
Wong Shouhao <wongshouhao@outlook.com>
2022-08-02 20:14:23 +0800

    Add metamask.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-// import HelloWorld from './components/HelloWorld.vue'
+import Metamask from './components/metamask.vue';
 </script>
 
 <template>
 <div class="container top-gap">
   <div class="columns">
-    <div class="column is-one-quarter">is-one-quarter</div>
+    <div class="column is-one-quarter">
+      <Metamask />
+    </div>
     <div class="column">Auto</div>
     <div class="column">Auto</div>
   </div>
